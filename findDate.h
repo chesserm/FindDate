f2c6951c0fe46bc253434b560f660153972afd8f
Matthew Chesser <chesser.matthew@gmail.com>
2018-08-20 15:53:08 -0400

    Fixed bugs, program works now (check readme)

--- findDate.h
+++ findDate.h
@@ -3,7 +3,7 @@
  * https://github.com/chesserm
  * Last edited 8/20/2018
 */
-
+#include <string>
 #ifndef FIND_DATE_H
 #define FIND_DATE_H
 
@@ -12,26 +12,26 @@
 // MODIFIES: Nothing
 // EFFECTS:  returns an integer value corresponding to the
 //           number of days in the previous month
-int daysOfPreviousMonth(int currentMonth, int currentYear) {}
+int daysOfPreviousMonth(int currentMonth, int currentYear);
 
 
 // REQUIRES: currentMonth and currentYear are valid ints
 // MODIFIES: Nothing
 // EFFECTS:  returns an integer value corresponding to the
 //           number of days in the previous month
-int daysOfNextMonth(int currentMonth, int currentYear) {}
+int daysOfNextMonth(int currentMonth, int currentYear);
 
 
 // REQUIRES: currentMonth and currentYear are valid ints
 // MODIFIES: Nothing.
 // EFFECTS:  returns an integer value corresponding to the
 //           number of days in the current month
-int daysOfCurrentMonth(int currentMonth, int currentYear) {}
+int daysOfCurrentMonth(int currentMonth, int currentYear);
 
 
 // REQUIRES: currentMonth is an int between 1-12
 // MODIFIES: Nothing
 // EFFECTS:  Retruns a string of the month's name
-string findMonthString(int currentMonth) {}
+std::string findMonthString(int currentMonth);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 */
 
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include "findDate.h"
 using namespace std;
@@ -17,8 +18,8 @@ using namespace std;
 void introduction()
 {
 	cout << "                                      Find a Date" << endl;
-	cout << "This program calculates the date which is x days before (or after) the specified date." << endl;
 	cout << "=======================================================================================" << endl;
+	cout << "This program calculates the date which is x days before (or after) the specified date." << endl;
 	cout << endl;
 	cout << "The program will first ask you to enter a target date in the order of day, month, year." << endl;
 	cout << "For example, June 15, 2018 would be entered as:" << endl;
@@ -27,88 +28,12 @@ void introduction()
 	cout << "Year:   2018" << endl;
 	cout << endl;
 	cout << "Lastly, just enter the amount of days before/after the date you want to calculate." << endl;
-	cout << "======================================================================================="
+	cout << "=======================================================================================" << endl;
 	cout << endl;
 }
 
 
-// REQUIRES: Nothing.
-// MODIFIES: Nothing.
-// EFFECTS:  Gathers necessary data and calls calculateDate() to
-//           find result and print it.
-void gatherUserInput()
-{
-	cout << "Please enter the known date." << endl;
-	cout << "Day:   ";
-
-	int intKnownDay;
-	cin >> intKnownDay;
-
-	cout << "Month: ";
-
-	int intKnownMonth;
-	cin >> intKnownMonth;
-
-	cout << "Year:  ";
-
-	int intKnownYear;
-	cin >> intKnownYear;
-
-	cout << endl;  //Formatting
-
-	// Determining if the entered year is a leap year (and if date is valid)
-	bool isLeapYear = intKnownYear % 4 == 0;
-    // If it's not a leapyear and a day in April is greater than 28
-    bool isAprilCase1 = !isLeapYear && intKnownMonth == 2 && intKnownDay > 28;
-    // If it is a leapyear and a day in April is greater than 29
-    bool isAprilCase2 = isLeapYear && intKnownMonth == 2 && intKnownDay > 29;
-
-    // Input testing
-    while (isAprilCase1 || isAprilCase2 || intKnownMonth > 12 || intKnownMonth < 1 || intKnownDay < 1 || intKnownDay > 31 || intKnownYear < 0)
-    {
-    	cout << "Invalid date entered." << endl;
-
-    	cout << "Please enter the known date." << endl;
-		cout << "Day:   ";
-		cin >> intKnownDay;
-
-		cout << "Month: ";
-		cin >> intKnownMonth;
-
-		cout << "Year:  ";
-		cin >> intKnownYear;
-
-		cout << endl;  //Formatting
-    }
-
-	cout << "Would you like to find a new date before or after the entered date? ";
-	string strBeforeAfter;
-	cin >> strBeforeAfter;
-	
-	// Testing user input
-	while (strBeforeAfter.tolower() != "before" || strBeforeAfter.tolower() != "after")
-	{
-		cout << "Please enter either \"before\" or \"after\"." << endl;
-		cout << "Would you like to find a new date before or after the entered date? ";
-		cin >> strBeforeAfter;
-	}
-	
-	cout << "How many days " << strBeforeAfter << " the entered date would you like to calculate? ";
-	int intTotalDays;
-	cin >> intTotalDays;
-
-	// Formatting and ouputting the results
-	cout << "";
-	cout << "========================================================================================" << endl;
-	cout << "The date " << intTotalDays << " days " << strBeforeAfter << " " << findMonthString(intKnownMonth) << ", " << intKnownYear << "is: ";
-
-	// Ouput of the desired date is handled within the calculateDate() function
-	calculateDate(intKnownDay, intKnownMonth, intKnownYear, intTotalDays, strBeforeAfter); 
-
-}
-
-
-// REQURIES: valid ints and string values
+// REQURIES: valid ints and string. String must be lower case.
 // MODIFIES: Nothing.
 // EFFECTS:  Calculates and ouputs desired date
 void calculateDate(int intKnownDay, int intKnownMonth, int intKnownYear, int intTotalDays, string& strBeforeAfter)
@@ -119,7 +44,7 @@ void calculateDate(int intKnownDay, int intKnownMonth, int intKnownYear, int int
     while (intTotalDays > 0)
     {
         // Wanting to find a date before entered day
-        if (strBeforeAfter.tolower() == "before")
+        if (strBeforeAfter == "before")
         {
             // If the known day is not 1
             if (intKnownDay > 1)
@@ -183,6 +108,88 @@ void calculateDate(int intKnownDay, int intKnownMonth, int intKnownYear, int int
 
     // Ouput result
     cout << findMonthString(intKnownMonth) << " " << intKnownDay << ", " << intKnownYear << " (" << intKnownMonth << "/" << intKnownDay << "/" << intKnownYear << ")." << endl;
+
+}
+
+
+// REQUIRES: Nothing.
+// MODIFIES: Nothing.
+// EFFECTS:  Gathers necessary data and calls calculateDate() to
+//           find result and print it.
+void gatherUserInput()
+{
+	cout << "Please enter the known date." << endl;
+	cout << "Day:   ";
+
+	int intKnownDay;
+	cin >> intKnownDay;
+
+	cout << "Month: ";
+
+	int intKnownMonth;
+	cin >> intKnownMonth;
+
+	cout << "Year:  ";
+
+	int intKnownYear;
+	cin >> intKnownYear;
+
+	cout << endl;  //Formatting
+
+	// Determining if the entered year is a leap year (and if date is valid)
+	bool isLeapYear = intKnownYear % 4 == 0;
+    // If it's not a leapyear and a day in April is greater than 28
+    bool isAprilCase1 = !isLeapYear && intKnownMonth == 2 && intKnownDay > 28;
+    // If it is a leapyear and a day in April is greater than 29
+    bool isAprilCase2 = isLeapYear && intKnownMonth == 2 && intKnownDay > 29;
+
+    // Input testing
+    while (isAprilCase1 || isAprilCase2 || intKnownMonth > 12 || intKnownMonth < 1 || intKnownDay < 1 || intKnownDay > 31 || intKnownYear < 0)
+    {
+    	cout << "Invalid date entered." << endl;
+
+    	cout << "Please enter the known date." << endl;
+		cout << "Day:   ";
+		cin >> intKnownDay;
+
+		cout << "Month: ";
+		cin >> intKnownMonth;
+
+		cout << "Year:  ";
+		cin >> intKnownYear;
+
+		cout << endl;  //Formatting
+    }
+
+	cout << "Would you like to find a new date before or after the entered date? ";
+	string strBeforeAfter;
+	cin >> strBeforeAfter;
+	
+	// Forcing string to lower case to test value
+	transform(strBeforeAfter.begin(), strBeforeAfter.end(), strBeforeAfter.begin(), ::tolower);
+	
+	// Testing user input
+	while ( (strBeforeAfter != "before") && (strBeforeAfter != "after") )
+	{
+		cout << "Please enter either \"before\" or \"after\"." << endl;
+		cout << "Would you like to find a new date before or after the entered date? ";
+		cin >> strBeforeAfter;
+
+		// Forcing string to lower case to test value
+		transform(strBeforeAfter.begin(), strBeforeAfter.end(), strBeforeAfter.begin(), ::tolower);
+	}
+	
+	cout << "How many days " << strBeforeAfter << " the entered date would you like to calculate? ";
+	int intTotalDays;
+	cin >> intTotalDays;
+
+	// Formatting and ouputting the results
+	cout << "";
+	cout << "========================================================================================" << endl;
+	cout << "The date " << intTotalDays << " days " << strBeforeAfter << " " << findMonthString(intKnownMonth) << " " << intKnownDay << ", " << intKnownYear << " is: ";
+
+	// Ouput of the desired date is handled within the calculateDate() function
+	calculateDate(intKnownDay, intKnownMonth, intKnownYear, intTotalDays, strBeforeAfter); 
 
 }
 
